1012a48664b977fc9c44ac3729ef8dd60ef5999f
block-bike <bang98.2016@gmail.com>
2019-07-14 15:22:09 +0700

    upload vipham

--- api/Model/loi_Model.js
+++ api/Model/loi_Model.js
@@ -1,40 +1,40 @@
-// const Sequelize = require("sequelize");
-// const db = require("./../Config/db");
-
-// const loi_Model = db.define("loi", {
-//   LOI_ID: {
-//     type: Sequelize.STRING,
-//     primaryKey: true,
-//     allowNull: false
-//   },
-//   LOI_TEN: {
-//     type: Sequelize.STRING
-//   },
-//   LOI_MOTA: {
-//     type: Sequelize.STRING
-//   }
-// });
-
-// db.sync();
-// module.exports = loi_Model;
-
 const Sequelize = require("sequelize");
 const db = require("./../Config/db");
 
-const khuonvien_Model = db.define("khuonvien", {
-  KV_ID: {
+const loi_Model = db.define("loi", {
+  LOI_ID: {
     type: Sequelize.STRING,
     primaryKey: true,
-    // autoIncrement: true,
     allowNull: false
   },
-  KV_TOADO: {
+  LOI_TEN: {
     type: Sequelize.STRING
   },
-  KV_TRANGTHAI: {
+  LOI_MOTA: {
     type: Sequelize.STRING
   }
 });
 
 db.sync();
-module.exports = khuonvien_Model;
+module.exports = loi_Model;
+
+// const Sequelize = require("sequelize");
+// const db = require("./../Config/db");
+
+// const khuonvien_Model = db.define("khuonvien", {
+//   KV_ID: {
+//     type: Sequelize.STRING,
+//     primaryKey: true,
+//     // autoIncrement: true,
+//     allowNull: false
+//   },
+//   KV_TOADO: {
+//     type: Sequelize.STRING
+//   },
+//   KV_TRANGTHAI: {
+//     type: Sequelize.STRING
+//   }
+// });
+
+// db.sync();
+// module.exports = khuonvien_Model;
