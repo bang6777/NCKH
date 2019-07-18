8d4c8e440941f1c87ac3ce17d95633a2bb40af36
unknown <thuythao1912@gmail.com>
2019-07-18 11:52:34 +0700

    add db

--- api/Config/db.js
+++ api/Config/db.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+module.exports = new Sequelize(process.env.DATABASE_URL);
 
 // // Option 1: Passing parameters separately : CSDL ONLINE
 // module.exports = new Sequelize("sql12297830", "sql12297830", "UI2y5fJwMQ", {
@@ -15,20 +16,20 @@ const Sequelize = require("sequelize");
 //   }
 // });
 
-// Option 2: Passing parameters separately : CSDL OFFLINE
-module.exports = new Sequelize("xedap", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  define: {
-    freezeTableName: true
-  }
-});
+// // Option 2: Passing parameters separately : CSDL OFFLINE
+// module.exports = new Sequelize("xedap", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+//   define: {
+//     freezeTableName: true
+//   }
+// });
 
 // if (!global.hasOwnProperty('db')) {
 //   var Sequelize = require('sequelize')
--- public/js/khuonvien-2.js
+++ public/js/khuonvien-2.js
@@ -65,34 +65,174 @@ function drawKhuonVien(coordinates) {
     //editable : true
   });
   polygon.setMap(map);
-  alert(coordinates.length);
-  alert("da ve");
+  // alert(coordinates.length);
+  // alert("da ve");
 }
 
-//get toa do
-// function GetAndDraw() {
-//   coordinates = [];
-//   // alert(coordinates.length + "lenght-----");
-//   $.ajax({
-//     method: "GET",
-//     url: "/khuonvien/getToaDo",
-//     contentType: "application/json",
-//     success: function(response) {
-//       // console.log(response);
-//       if (response.length > 0) {
-//         $.each(response, function(i, td) {
-//           coordinates.push({ lat: td.KV_LAT, lng: td.KV_LNG });
-//         });
+// get toa do
+function GetToaDo() {
+  coordinates = [];
+  $.ajax({
+    method: "GET",
+    url: "/khuonvien/getToaDo",
+    contentType: "application/json",
+    success: function(response) {
+      // console.log(response);
+      if (response.length > 0) {
+        $.each(response, function(i, td) {
+          coordinates.push({ lat: td.KV_LAT, lng: td.KV_LNG });
+        });
+        alert("get xong");
+      } else {
+        alert("Hien k có tọa độ");
+      }
+    },
+    error: function(e) {
+      alert("Đã có lỗi xảy ra!");
+      console.log(e);
+    }
+  });
+}
 
-//         drawKhuonVien(coordinates);
-//         // alert(coordinates.length + "-----");
-//       } else {
-//         alert("Hien k có tọa độ");
-//       }
-//     },
-//     error: function(e) {
-//       alert("Đã có lỗi xảy ra!");
-//       console.log(e);
-//     }
-//   });
-// }
+//------------Button cap nhat
+//Cap nhat khuon vien
+async function updateKhuonVien() {
+  document.getElementById("btnEditMarker").disabled = true;
+
+  //get toa do
+  coordinates = [];
+  markers = [];
+  await $.ajax({
+    method: "GET",
+    url: "/khuonvien/getToaDo",
+    contentType: "application/json",
+    success: function(response) {
+      // console.log(response);
+      if (response.length > 0) {
+        $.each(response, function(i, td) {
+          coordinates.push({ lat: td.KV_LAT, lng: td.KV_LNG });
+        });
+
+        //dat marker
+        if (coordinates.length > 0) {
+          for (i = 0; i < coordinates.length; i++) {
+            placeMarker(coordinates[i]);
+          }
+          polygon.setMap(null);
+        }
+      } else {
+        alert("Hien k có tọa độ");
+      }
+    },
+    error: function(e) {
+      alert("Đã có lỗi xảy ra!");
+      console.log(e);
+    }
+  });
+  map.addListener("click", function(e) {
+    placeMarker(e.latLng, map);
+  });
+}
+
+//--------------Button Hoan Thanh
+async function Save() {
+  google.maps.event.clearListeners(map, "click");
+  setMapOnAll(null);
+  //luu toa do
+  coordinates = [];
+  var j = 0;
+  document.getElementById("ToaDo").innerHTML = "";
+
+  //cap nhat
+  await $.ajax({
+    url: "/khuonvien/update",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({ KV_TRANGTHAI: "0" }),
+    success: function() {
+      alert("Đã cập nhật thành công trạng thái tọa độ!");
+    },
+    error: function(e) {
+      alert("Đã có lỗi xảy ra!");
+      console.log(e);
+    }
+  });
+
+  //luu
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setDraggable(false);
+    if (markers[i].label != "null") {
+      document.getElementById("ToaDo").innerHTML +=
+        markers[i].id + "-" + "Lat: " + markers[i].position.lat() + " - Lng: " + markers[i].position.lng() + "<br>";
+      kv_lat = markers[i].position.lat();
+      kv_lng = markers[i].position.lng();
+
+      //ajax luu
+      await $.ajax({
+        url: "/khuonvien",
+        method: "POST",
+        data: JSON.stringify({
+          KV_LAT: kv_lat,
+          KV_LNG: kv_lng
+        }),
+        contentType: "application/json",
+        success: function() {
+          alert("Đã thêm thành công tọa độ! " + kv_lat + "---" + kv_lng);
+        },
+        error: function(e) {
+          alert("Đã có lỗi xảy ra!");
+          console.log(e);
+        }
+      });
+    }
+  }
+
+  document.getElementById("btnEditMarker").disabled = false;
+
+  await load();
+}
+
+//6. Xoa tat ca marker
+function clearMarkers() {
+  setMapOnAll(null);
+  markers = [];
+  coordinates = [];
+  id = 0;
+}
+
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function placeMarker(position) {
+  if (!markers[id]) {
+    marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      label: id.toString(),
+      id: id,
+      draggable: true,
+      animation: google.maps.Animation.DROP
+    });
+
+    markers[id] = marker;
+    map.panTo(position);
+    id++;
+  } else {
+    id++;
+    placeMarker(position);
+  }
+
+  google.maps.event.addListener(marker, "rightclick", function(point) {
+    id = this.id;
+    delMarker(id);
+  });
+
+  var delMarker = function(id) {
+    marker = markers[id];
+    markers[id].setMap(null);
+    markers[id].label = "null";
+  };
+}
