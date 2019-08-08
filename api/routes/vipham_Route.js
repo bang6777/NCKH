4d384d78c39075a331ec80ed8eabf0e8d98c0fa9
unknown <thuythao1912@gmail.com>
2019-08-08 22:34:55 +0700

    xu ly vi pham - trang thai xe

--- api/routes/vipham_Route.js
+++ api/routes/vipham_Route.js
@@ -1,5 +1,6 @@
 var viphamCtr = require("../controller/vipham_Ctr");
 var xeCtr = require("../controller/xe_Ctr");
+var muontraCtr = require("../controller/muontra_Ctr");
 
 exports.viewTaiKhoan = function(req, res) {
   var TK_ID = req.params.TK_ID;
@@ -41,7 +42,10 @@ exports.updateXuLy = function(req, res) {
             xe_id = data.muontra.xeXEID;
             //cập nhật trạng thái xe = 0
             xeCtr.updateTrangThaiXe(xe_id, 0, function(err, data) {
-              res.json(data);
+              //kết thúc mượn trả
+              muontraCtr.updateTra_ThoiGian(MUONTRA_ID, function(err, data) {
+                res.json(data);
+              });
             });
           }
         }
